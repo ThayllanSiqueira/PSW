8bff98f6fea19d525de309d01aa6d602d0830b56
RA21551794 <RA21551794@T1-120422-57301.dinfor.uniceub>
2016-09-20 20:50:33 -0300

    Receitas aula 3 Aprendendo Banco de Dados

---
+++ Receitas/src/java/IncluirIngrediente.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author RA21551794
+ */
+public class IncluirIngrediente extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       
+        try {
+            //Carrega o driver na memória
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            
+            // Conecta ao banco
+            Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/sample","app", "app");
+            
+            // Cria o comando
+            java.sql.Statement comando = con.createStatement();  
+            
+            String nome = request.getParameter("nome"); 
+            String calorias = request.getParameter("calorias"); 
+            
+            String query = "insert into ingrediente values ('" + nome + "', " + calorias + ")";
+            
+            //executa um update
+            int linhas = comando.executeUpdate(query);
+            
+            request.getRequestDispatcher("sucesso.jsp").forward(request, response);
+
+        } catch (Exception ex) {
+            
+            request.setAttribute("erro", ex.getMessage());
+            
+            request.getRequestDispatcher("erro.jsp").forward(request, response);
+        }
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
